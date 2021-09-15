a7aaf01da5cea53c16c8956a87e630966963d79f
Davide D Amico <3356631+davidone@users.noreply.github.com>
2021-09-15 12:06:55 +0200

    Update diff_between_times.py

--- diff_between_times.py
+++ diff_between_times.py
@@ -4,21 +4,21 @@ import sys
 import re
 import click
 
-
+FMT = "%Y-%m-%d %H:%M:%S"
+ 
 def validate_date(ctx, param, value):
     if isinstance(value, tuple):
         return value
-
-    p = re.compile("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01]) ([01][0-9]|2[0-3])(:[0-5][0-9]){2}$")
-    if p.match(value) is None:
+    try:
+        datetime.strptime(value, FMT)
+    except ValueError as err:
         raise click.BadParameter("format must be '%Y-%m-%d %H:%M:%S'")
     return value
 
 
 def dateDiff(since, until) -> int:
-    fmt = "%Y-%m-%d %H:%M:%S"
-    d1 = datetime.strptime(since, fmt)
-    d2 = datetime.strptime(until, fmt)
+    d1 = datetime.strptime(since, FMT)
+    d2 = datetime.strptime(until, FMT)
 
     # Convert to Unix timestamp
     d1_ts = time.mktime(d1.timetuple())
